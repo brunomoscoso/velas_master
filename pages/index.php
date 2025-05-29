35d811391e9d300d325a3c5ba853c9edb5df02df
brunomoscoso <158348569+brunomoscoso@users.noreply.github.com>
2025-05-29 16:50:27 +0100

    Nav

--- pages/index.php
+++ pages/index.php
@@ -16,7 +16,91 @@ $result = $conn->query($sql);
 <body>
 
 <header class="bg-white py-2">
-  <!-- ... HEADER COMPLETO COMO ESTAVA ... -->
+<div class="container-fluid">
+    
+    <!-- Linha para mobile: logo à esquerda e botão à direita -->
+    <div class="d-flex d-lg-none justify-content-between align-items-center">
+      <!-- Logo -->
+      <a href="#" class="logo-mobile">
+        <img src="../assets/images/logo/moscoso.png" alt="Logo" class="img-mobile-logo">
+      </a>
+      <!-- Botão hamburguer -->
+      <div class="container-mobile-icons">
+      <a class="nav-link-menu" href="#"><img src="../assets/images/icons/person_24dp_000000_FILL0_wght400_GRAD0_opsz24.png" alt=""></a>
+      <a class="nav-link-menu" href="#"><img src="../assets/images/icons/shopping_bag_24dp_000000_FILL0_wght400_GRAD0_opsz24.png" alt=""></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+          <img src="../assets/images/icons/menu_24dp_434343_FILL0_wght400_GRAD0_opsz24 (1).svg" alt="" class="botao-mobile">
+        </button>
+      </div>
+    </div>
+
+    <!-- Linha para telas médias e grandes -->
+    <div class="row align-items-center d-none d-lg-flex text-center">
+
+      <!-- Menu lateral -->
+      <div class="col-lg-3 coluna">
+        <div class="menu d-flex justify-content-start gap-3">
+          <a href="#" class="link-menu img-icons">COLEÇÃO</a>
+          <a href="#" class="link-menu img-icons">PERSONALIZAÇÃO</a>
+        </div>
+      </div>
+
+      <!-- Logo central -->
+      <div class="col-lg-5">
+        <a href="#" id="logo">
+          <img id="img-logo" class="img-fluid" src="../assets/images/logo/moscoso.png" alt="Logo">
+        </a>
+      </div>
+
+      <!-- Ícones -->
+      <div class="col-lg-3 coluna">
+        <div class="icons d-flex justify-content-end gap-3">
+          <a href="#" class="img-icons"><img src="../assets/images/icons/person_24dp_000000_FILL0_wght400_GRAD0_opsz24.png" alt=""></a>
+          <a href="#" class="img-icons"><img src="../assets/images/icons/favorite_24dp_000000_FILL0_wght400_GRAD0_opsz24.png" alt=""></a>
+          <a href="#" class="img-icons"><img src="../assets/images/icons/shopping_bag_24dp_000000_FILL0_wght400_GRAD0_opsz24.png" alt=""></a>
+          <a href="#" class="img-icons"><img src="../assets/images/icons/search_24dp_000000_FILL0_wght400_GRAD0_opsz24.png" alt=""></a>
+        </div>
+      </div>
+
+      <!-- Botão de menu (aparece também no desktop) -->
+      <div class="col-lg-1 coluna">
+        <nav class="navbar bg-white justify-content-end p-0">
+          <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </nav>
+      </div>
+    </div>
+  </div>
+
+  <!-- MENU OFFCANVAS -->
+  <div class="offcanvas offcanvas-end text-bg-white" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+    <div class="offcanvas-header">
+      <img class="img-logo-nav" src="../assets/images/logo/moscoso.png" alt="Logo Offcanvas">
+      <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Fechar"></button>
+    </div>
+
+    <div class="offcanvas-body">
+      
+      <!-- CONTEÚDO DESKTOP -->
+      <div class="d-none d-lg-block">
+        <a class="nav-link" href="#">Quem Somos</a>
+        <a class="nav-link" href="#">Grupo Moscoso</a>
+        <a class="nav-link" href="#">Contactos</a>
+      </div>
+
+      <!-- CONTEÚDO MOBILE -->
+      <div class="d-block d-lg-none">
+        <a class="nav-link" href="#">COLEÇÃO</a>
+        <a class="nav-link" href="#">PERSONALIZAÇÃO</a>
+        <a class="nav-link" href="#"><img src="../assets/images/icons/person_24dp_000000_FILL0_wght400_GRAD0_opsz24.png" alt=""> Conta</a>
+        <a class="nav-link" href="#"><img src="../assets/images/icons/shopping_bag_24dp_000000_FILL0_wght400_GRAD0_opsz24.png" alt=""> Sacola</a>
+        <a class="nav-link" href="#"><img src="../assets/images/icons/search_24dp_000000_FILL0_wght400_GRAD0_opsz24.png" alt=""> Buscar</a>
+        <hr>
+      </div>
+
+    </div>
+  </div>
 </header>
 
 <div>
